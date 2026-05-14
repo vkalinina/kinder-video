bfd0d3b12e8c7fcd1be5392d52d2c31725a39249
Viktoria Kalinina <viktoriakalinina@orderberry.de>
2026-05-14 16:40:20 +0200

    add urls for app catalog

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path, include
-from rest_framework import routers
-from catalog.views import CategoryListView
+from rest_framework.routers import DefaultRouter
+from .views import CategoryViewSet, VideoViewSet, ChildProfileViewSet, WatchHistoryViewSet
 
 app_name = "catalog"
+
+router = DefaultRouter()
+router.register(r"categories", CategoryViewSet, basename="category")
+router.register(r"videos", VideoViewSet, basename="video")
+router.register(r"children", ChildProfileViewSet, basename="childprofile")
+router.register(r"watch-history", WatchHistoryViewSet, basename="watchhistory")
+
 urlpatterns = [
-    path(
-        "categories/",
-        CategoryListView.as_view(),
-        name="category-list"
-    ),
+    path("", include(router.urls)),
 ]
